c83cc9022c4d49c98019f9f3651c28b72627cadb
qianyang.abcft <1287124934@qq.com>
2023-12-15 17:18:03 +0800

    springamqp练习

---
+++ src/test/java/com/rabbitmqtest/producespring/ProduceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmqtest.producespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduceSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
